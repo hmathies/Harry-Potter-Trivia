0542966dc6c38e2dc1793dfc8bb482c9378af104
Heather Mathies <heathermathies@Heathers-MacBook-Air.local>
2017-08-24 17:28:48 -0400

    ready for grading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
-/*----when the page loads, the trivia div will be displayed with title and start button only*/
-	  $(document).ready(function() {
-          var timeRemaining = 60;
+ /*----when the page loads, the trivia div will be displayed with title and start button only*/
+    $(document).ready(function() {
+          var timeRemaining = 10;
           var timerId;
           var correct = 0;
           var incorrect = 0;
           var unanswered = 0; 
 
          // displaying the results to the screen
-           function displayResults () {
+          function displayResults () {
               $("#resultsPage").show();
               $('#questionPage').hide();
            }
@@ -15,14 +15,14 @@
           $("#questionPage").hide();
           $("#resultsPage").hide();
       // when the button is clicked, load the trivia questions and begin countdown timer
-    			$('.btn').on("click", function() {
+          $('.btn').on("click", function() {
               $("#startPage").hide();
               $("#questionPage").show();
               run(); 
           });
           /*this function starts the timer*/
           function run() {
-          timerId = setInterval(decrement, 1000);
+            timerId = setInterval(decrement, 1000);
           }
           /*this function has the timer count down*/
           function decrement(){
@@ -33,7 +33,7 @@
             stop();
           } 
          }
-        
+        /*if the user clicks the submit button*/
           $('#PotterForm').submit(function( event ) {
               console.log( "working" );
               event.preventDefault();
@@ -41,7 +41,7 @@
               stop();
           });
       
-            
+            /*this function checks the answers and records them to the results page*/
           function checkAnswers(){
             var q1 = $("input[type='radio'][name='underwater']:checked").val();
             var q2 = $("input[type='radio'][name='joke']:checked").val();
@@ -50,116 +50,45 @@
             var q5 = $("input[type='radio'][name='guards']:checked").val();
             var q6 = $("input[type='radio'][name='Ron']:checked").val();
             var q7 = $("input[type='radio'][name='Phoenix']:checked").val();
-            if (q1 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: ' + $("#correct").html("<h3>" + "Correct: " + correct + "</h3>"));
-            }
-            else if (q1 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-               $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-                if (q2 === 'right') {
-              correct++;
+            
+               $.each([ q1, q2, q3, q4, q5, q6, q7 ], function( index, value ) {
+             if (value === 'right') {
+               correct++;
                $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q2 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-            if (q3 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q3 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-               if (q4 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q4 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-               if (q5 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q5 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-               if (q6 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q6 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
-               if (q7 === 'right') {
-              correct++;
-              $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
-              console.log('right: '+ correct);
-            }
-            else if (q7 == undefined) {
-              unanswered++;
-              $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
-              console.log('unanswered: '+ unanswered);
-            }
-            else{
-              incorrect++;
-              $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
-              console.log('incorrect: '+ incorrect);
-            }
+               console.log('right: ' + $("#correct").html("<h3>" + "Correct: " + correct + "</h3>"));
+             }
+             else if (value == undefined) {
+               unanswered++;
+               $("#unanswered").html("<h3>" + "Unanswered: " + unanswered + "</h3>");
+               console.log('unanswered: '+ unanswered);
+             }
+             else{
+               incorrect++;
+                $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
+               console.log('incorrect: '+ incorrect);
+             }
+           });
 
            }
            function stop() {
               clearInterval(timerId);
               displayResults();
+              // timerId = setInterval(decrement, 1000);
+              setTimeout(reset,1000*5);
+              console.log(setTimeout);
+           }
+           
+           function reset() {
+              $('#startPage').show();
+              $("#resultsPage").hide();
+              clearInterval(timerId);
+              console.log('working');
+              // $('#questionPage').clear();
+              timeRemaining = 10;
+              timerId;
+              correct = 0;
+              incorrect = 0;
+              unanswered = 0; 
            }
            
     });
